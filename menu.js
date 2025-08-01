59b3e01839a7762276e4d6f1b139c450ee39f1f1
kasmbafumoja <kaskas2008910@gmail.com>
2025-08-01 15:49:05 +0200

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,43 @@
+export const menu = `
+╔═══⚡═══╗
+║ 🌐 TERMUX-MD 🌐
+╚═══⚡═══╝
+
+┏━━━━━━━◈
+┃ 👑 Owner : KAS Mbafumoja
+┃ 📌 Préfix : .
+┃ 🌍 Mode : Private
+┃ 🌀 Version : 1.7.8
+┃ ⚡ Vitesse : Ultra-Rapide
+┃ 📦 Plugins : 309
+┃ 🛠️ Hébergeur : Panel Render
+┗━━━━━━━◈
+
+┏▣ ◈  *COMMANDES DISPONIBLES* ◈
+┃ Tape .menu pour explorer 
+┃ chaque catégorie 👇
+┗▣ 
+
+📂 *Catégories*
+╭──────────────╮
+│ 🤖 AI MENU
+│ 🎶 AUDIO MENU
+│ ⬇️ DOWNLOAD MENU
+│ 🎨 EPHOTO360 MENU
+│ 😂 FUN MENU
+│ 👥 GROUP MENU
+│ 🖼️ IMAGE MENU
+│ ⚙️ OTHER MENU
+│ 👑 OWNER MENU
+│ 📖 RELIGION MENU
+│ 🔍 SEARCH MENU
+│ 🛠️ SETTINGS MENU
+│ 🏆 SPORTS MENU
+│ 💡 SUPPORT MENU
+│ 🔧 TOOLS MENU
+│ 🎥 VIDEO MENU
+╰──────────────╯
+
+⚡ Tape *.<commande>* pour exécuter.
+Par exemple : *.ping* ou *.quran*
+`
