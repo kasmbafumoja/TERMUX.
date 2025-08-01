c2f6476f469ae39891357fa9b9f9d4a1956b9f73
kasmbafumoja <kaskas2008910@gmail.com>
2025-08-01 15:44:02 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+export default {
+  ownerName: "KAS TERMUX-MD 😇",
+  ownerNumber: ["2438xxxxxxx"], // Mets ton numéro WhatsApp
+  botName: "TERMUX-MD",
+  prefix: ".",
+  mode: "private", // options: "public" ou "private"
+  version: "1.7.8",
+  plugins: 309
+}
